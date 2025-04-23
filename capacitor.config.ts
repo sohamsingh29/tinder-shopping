bd31a39d8ae3029f39e15379015e59c9959a736e
sohamsingh29 <sohamsingh29@gmail.com>
2025-04-23 09:21:44 +0530

    added capacitor js

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tinder_shoppng.app',
+  appName: 'tinder-shopping',
+  webDir: 'dist'
+};
+
+export default config;
